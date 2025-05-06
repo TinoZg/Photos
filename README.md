98785960ff5576987faaf24d2140710d5214816d
tinozg <valentino.novakk@gmail.com>
2025-05-06 22:48:49 +0200

    Videos support and custom output directory

--- README.md
+++ README.md
@@ -1,17 +1,14 @@
 # Photos
 
-Node.js application that creates folder structure from photos and moves them in corresponding folder according to time a photo was taken.
+Node.js application that creates folder structure from photos/videos and moves them in corresponding folder according to time a photo was taken, e.g 2025-01.
 
 ## Getting started
 
-1. Put all unsorted images in the directory specified by the variable `photosDirectory` inside `index.js`. The code doesn't work recursively i.e. all photos must be in the specified path. Example for specifying the path:
-
-```js
-const photosDirectory = path.join('E:/', 'SomeDirectory', 'file.jpg');
-// Outputs: E:\SomeDirectory\file.jpg on Windows
-console.log(photosDirectory);
-```
-
+1. Put all unsorted images and videos in some directory. The code doesn't work recursively i.e. all photos must be in the specified path and there should be no subfolders
 2. Make sure you have [node.js](https://nodejs.org/en/download/package-manager) installed. Code is tested with v20.15.0
 3. `cd` to working directory and run `npm install`
-4. Run the code with `node index.js`
+4. Run the code with the following command where `inputDir` is the folder with unsorted photos/videos and `outputDir` is the destination folder
+
+```bash
+node index.js inputDir outputDir
+```
--- index.js
+++ index.js
@@ -2,12 +2,18 @@ const fs = require('fs');
 const path = require('path');
 const exifParser = require('exif-parser');
 
-const supportedExtensions = ['.jpg', '.jpeg', '.png'];
-const photosDirectory = path.join('E:/');
+const supportedExtensions = ['.jpg', '.jpeg', '.png', '.mp4', '.mov'];
+const photosDirectory = process.argv[2];
+
+const targetDirectory = process.argv[3];
 
 // Function to get the date from EXIF data
 const getPhotoDate = (filePath) => {
-  if (path.extname(filePath).toLowerCase() !== '.png') {
+  const ext = path.extname(filePath).toLowerCase();
+
+  // Only jpg and jpeg files are processed for EXIF data
+  // Other formats will use the file system date
+  if (ext === '.jpg' || ext === '.jpeg') {
     const buffer = fs.readFileSync(filePath);
     const parser = exifParser.create(buffer);
     const result = parser.parse();
@@ -32,7 +38,7 @@ const getPhotoDate = (filePath) => {
     const systemMonth = ('0' + (systemDate.getMonth() + 1)).slice(-2);
     return `${systemYear}-${systemMonth}`;
   }
-  throw new Error('No date found for the picture!!!');
+  throw new Error('No date found for the picture or video!!!');
 };
 
 // Function to move a file to a new directory
@@ -45,18 +51,19 @@ const moveFile = (filePath, targetDir) => {
   fs.renameSync(filePath, targetPath);
 };
 
-// Function to process the files in the photos directory
-const processFiles = (dir) => {
-  fs.readdirSync(dir).forEach((file) => {
-    const filePath = path.join(dir, file);
+// Function to process the files in the source directory and move them to the specified target directory
+const processFiles = (sourceDir, targetDir) => {
+  fs.readdirSync(sourceDir).forEach((file) => {
+    const filePath = path.join(sourceDir, file);
     if (supportedExtensions.includes(path.extname(file).toLowerCase())) {
       const photoDate = getPhotoDate(filePath);
       if (photoDate) {
-        const targetDir = path.join(dir, photoDate);
-        moveFile(filePath, targetDir);
+        // Create subfolders under the target directory based on the photo date
+        const destinationDir = path.join(targetDir, photoDate);
+        moveFile(filePath, destinationDir);
       }
     }
   });
 };
 
-processFiles(photosDirectory);
+processFiles(photosDirectory, targetDirectory);
